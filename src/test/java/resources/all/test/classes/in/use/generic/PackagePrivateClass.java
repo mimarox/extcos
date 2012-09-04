07cb9181775364f454c8db6168c85e0839b7e7b8
Matthias Rothe <mimarox@users.sourceforge.net>
2012-09-04 09:40:02 +0000

    updated test resources

---
+++ src/test/java/resources/all/test/classes/in/use/generic/PackagePrivateClass.java
@@ -0,0 +1,5 @@
+package resources.all.test.classes.in.use.generic;
+
+class PackagePrivateClass {
+	public PackagePrivateClass(){}
+}
---
+++ src/test/java/resources/all/test/classes/in/use/generic/TestClass.java
@@ -0,0 +1,11 @@
+package resources.all.test.classes.in.use.generic;
+
+import resources.annotations.TestInvokable;
+
+public class TestClass {
+	public ClassWithNestedClass clazz;
+	
+	@TestInvokable
+	public void testMethod() {
+	}
+}
